7731a8e8b208312522ffd6a8cbe2b9181ce2b900
Mohammed Ismail <mohammedismail523261@gmail.com>
2025-10-12 14:37:42 +0530

    gh pages

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -240,7 +240,7 @@ const Home = () => {
       </section>
 
 
-      {/* Whole Fruit Delivery Carousel  Section */}
+      {/* Whole Fruit Delivery Carousel   Section */}
       <section className="py-20">
         <div className="container mx-auto px-4">
           <h2 className="text-4xl font-bold text-center mb-12">Whole Fruit Delivery</h2>
